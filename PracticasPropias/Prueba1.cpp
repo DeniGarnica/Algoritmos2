a80bd4265dfc85828405052c1e6e3a17b0e2c703
DeniGarnica <denisse.garnica@cimat.mx>
2021-02-13 03:46:03 -0600

    25% de los casos con un problema de memoria del caso 2

--- PracticasPropias/Prueba1.cpp
+++ PracticasPropias/Prueba1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-int main()
-{
-    cout<< "Holis Bolis"  << '\n';
-    return 0;
+
+int main(){
+  int n=0;
+  int a,b,c;
+  cin>>n;
+  for(int i=0; i<n; i++){
+    cin>>a>>b>>c;
+    int men = min(a, min(b,c));
+    int may = max(a, max(b,c));
+    if(a!=men && a!=may){
+      cout<<"Case 1: "<< a<<'\n';
+    }else{
+      if(b!=men && b!=may){
+        cout<<"Case 2: "<< b<<'\n';
+      }else{
+        cout<<"Case 3: "<< c<<'\n';
+      }
+    }
+  }
+  return 0;
 }
