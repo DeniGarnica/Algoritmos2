572cef6dfa4636c1f2f42179011d93fa90d6e7fa
DeniGarnica <denisse.garnica@cimat.mx>
2021-02-20 15:14:18 -0600

    Segment tree con actualizacion de hoja para operacvion maximo

--- Tareas/SegmentTree.cpp
+++ Tareas/SegmentTree.cpp
@@ -14,6 +14,7 @@ int operacion(int a, int b){
 void construir(int *tree, int *A, int vertex, int L, int R){ //A son nuestros valores iniciales
   if(L==R){ //Si son iguales, nos referimos a una hoja
     tree[vertex]=A[L];
+    A[L]=vertex; //de esta manera guardo en que posicion del tree quedaro cada hoja
   }else{
     int  nL = 2*vertex+1;
     int  nR = 2*vertex+2;
@@ -25,7 +26,7 @@ void construir(int *tree, int *A, int vertex, int L, int R){ //A son nuestros va
 
 int rango(int* tree, int vertex, int L, int R, int l, int r){ //[l,r] es el rango que se esta buscando
     if(l>R|| r < L) return -1; //si esta fuera del rango
-    if(L>= l|| R <= r) return tree[vertex]; //si esta completamente contenido
+    if(L>= l && R <= r) return tree[vertex]; //si esta completamente contenido
 
     //Si solo esta parcialmente contenido busca en los hijos
     int p1 = rango(tree, 2*vertex +1, L, (L+R)/2, l, r);
@@ -35,17 +36,15 @@ int rango(int* tree, int vertex, int L, int R, int l, int r){ //[l,r] es el rang
     return operacion(p1, p2);
 }
 
-/*int query(int l, int r){
-  return rango(0, 0, N, l, r);
-}*/
 
 void actualizaHoja (int* tree, int* A, int vertex, int k){
-  A[vertex]= k;
-  int padre = (vertex-1)/2;
+  tree[A[vertex]]= k;
+  int padre = (A[vertex]-1)/2;
+  tree[padre]= operacion(tree[2*padre+1], tree[2*padre+2]);
 
   do{
-    tree[padre]=operacion(tree[2*padre+1], tree[2*padre+2]);
     padre=(padre-1)/2;
+    tree[padre]= operacion(tree[2*padre+1], tree[2*padre+2]);
   }while(padre != 0);
 }
 
@@ -56,11 +55,22 @@ int main(){
   for (int i=0; i<n; i++) {
     std::cin >> A[i];
   }
-  int B[2*n];
-  construir(B, A, 0, 0, n-1);
+  int tree[2*n];
+  construir(tree, A, 0, 0, n-1);
   std::cout << "rango a buscar" << '\n';
   std::cin >> l >> r;
-  std::cout << rango(B, 0, 0, n, l, r)<< '\n';
+  std::cout << rango(tree, 0, 0, n-1, l, r)<< '\n';
+
+  actualizaHoja (tree, A, 2, 8);
+
+  /*std::cout << "Hojas del actualizado: " << '\n';
+  for (int i=0; i<n; i++) {
+    std::cout << tree[A[i]] << " ";
+  }*/
+
+  std::cout <<'\n' <<"rango a buscar del actualizado" << '\n';
+  std::cin >> l >> r;
+  std::cout << rango(tree, 0, 0, n-1, l, r)<< '\n';
 
   return 0;
 }
