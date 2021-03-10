718fef3113c708eacbc671ea3cbb05f33ecaecc6
DeniGarnica <denisse.garnica@cimat.mx>
2021-03-10 17:05:10 -0600

    st con datos correctos

--- Tareas/HeavyLight.cpp
+++ Tareas/HeavyLight.cpp
@@ -6,16 +6,17 @@ using namespace std;
 int operacion(int a, int b){
   return a+b;
 }
-void construir(vector<int> &tree, vector<int> A, int vertex, int L, int R){ //A son nuestros valores iniciales
+void construir(vector<int> &tree, int*A, vector<int> chain, int vertex, int L, int R){ //A son nuestros valores iniciales
   if(L==R){ //Si son iguales, nos referimos a una hoja
-    tree[vertex]=A[R];
+    tree[vertex]=A[chain[L]];
     //std::cout << "hoja: "<< tree[vertex] << '\n';
-    //A[L]=vertex; //de esta manera guardo en que posicion del tree quedaro cada hoja
+    A[L]=vertex; //de esta manera guardo en que posicion del tree quedaro cada hoja
+    std::cout << "El nodo "<< L<< " quedo en "<< vertex << '\n';
   }else{
     int  nL = 2*vertex+1;
     int  nR = 2*vertex+2;
-    construir(tree, A, nL, L, (L+R)/2); //construye el arbol izquierdo
-    construir(tree, A, nR, (L+R)/2 +1, R); //construye el arbol derecho
+    construir(tree, A, chain, nL, L, (L+R)/2); //construye el arbol izquierdo
+    construir(tree, A, chain,  nR, (L+R)/2 +1, R); //construye el arbol derecho
     tree[vertex] = operacion(tree[nL], tree[nR]); //asigna al nodo la operacion correspondiente de sus dos hijos
     //std::cout << "nodo: "<< tree[vertex] << '\n';
   }
@@ -31,9 +32,9 @@ int rango(vector<int> &tree, int vertex, int L, int R, int l, int r){ //[l,r] es
     if (p2== -1) return p1;
     return operacion(p1, p2);
 }
-void actualizaHoja (vector<int> &tree, vector<int> A, int vertex, int k){
-  tree[A[vertex]]= k;
-  int padre = (A[vertex]-1)/2;
+void actualizaHoja (vector<int> &tree, int vertex, int k){
+  tree[vertex]= k;
+  int padre = (vertex-1)/2;
   tree[padre]= operacion(tree[2*padre+1], tree[2*padre+2]);
 
   do{
@@ -57,9 +58,10 @@ int subsize(int i, vector<vector<int>> hijos, int *subarbolsize){ // con esta fu
     return subarbolsize[i];
   }
 }
-void chains(int i, int *subarbolsize, vector<vector<int>> hijos, vector<vector<int>> &chain, int* curr_chain){
+void chains(int i, int *subarbolsize, vector<vector<int>> hijos, vector<vector<int>> &chain, int* curr_chain, int* numChain){
 
   chain[curr_chain[0]].push_back(i);//se mete el nodo a la cadena actual
+  numChain[i]=curr_chain[0];//gaurdamos en que cadena esta
   //std::cout << "se agrego "<< i <<" a la cadena"<< curr_chain[0] << '\n';
   //Buscamos el hijo mas pesado
   if(!hijos[i].empty()){
@@ -71,12 +73,12 @@ void chains(int i, int *subarbolsize, vector<vector<int>> hijos, vector<vector<i
         nodoPesado=hijos[i][j];
       }
     }
-    chains(nodoPesado, subarbolsize, hijos,chain, curr_chain); //metemos el hijo mas pesado a la misma cadena
+    chains(nodoPesado, subarbolsize, hijos,chain, curr_chain, numChain); //metemos el hijo mas pesado a la misma cadena
 
     for (size_t j = 0; j < hijos[i].size(); j++) {
       if(hijos[i][j]!=nodoPesado){
         curr_chain[0]++; //cambiamos el numero de cadena
-        chains(hijos[i][j], subarbolsize, hijos, chain, curr_chain);//ponemos el resto de los hijos a otras cadenas
+        chains(hijos[i][j], subarbolsize, hijos, chain, curr_chain, numChain);//ponemos el resto de los hijos a otras cadenas
       }
     }
   }else{
@@ -87,15 +89,12 @@ void chains(int i, int *subarbolsize, vector<vector<int>> hijos, vector<vector<i
 
 
 int main(){
-  int n, q;
+  int n, q, cambio, valornuevo;
+  char peticion;
   std::cin >> n >> q;
-  int A[n];
-  int padre[n];
-  vector<vector<int>> hijos;
-  int subarbolsize[n];
+  int A[n]; int padre[n]; int numChain[n]; int subarbolsize[n];
+  vector<vector<int>> hijos; vector<vector<int>> st; vector<vector<int>> chain;
   padre[0]=0;
-  vector<vector<int>> st;
-  vector<vector<int>> chain;
   for (size_t i = 0; i < n; i++) {
     std::cin >> A[i];
     hijos.push_back({});
@@ -115,24 +114,31 @@ int main(){
     }
     std::cout  << '\n';
   }*/
-
   subsize(0, hijos, subarbolsize); //asigna los tama;os de cada subarbol en el arreglo subarbolsize
-
   int curr_chain[1]; //hice este arreglo de tam 1 para poder ir modificando cual es la cadena actual apesar de ser llamadas recursivas
   curr_chain[0]=0;
-  chains(0, subarbolsize, hijos, chain, curr_chain);//Creamos las cadenas
+  chains(0, subarbolsize, hijos, chain, curr_chain, numChain);//Creamos las cadenas
   int a = 0;
   while(!chain[a].empty()&&a<n){ // creamos los st de las cadenas
     vector<int> s (2*chain[a].size()-1, 0);
-    construir(s, chain[a], 0, 0, chain[a].size()-1);
+    construir(s, A, chain[a], 0, 0, chain[a].size()-1);
     st[a]=s;
     a++;
   }
-    //vector<int> s (2*chain[0].size()-1, 0);
-    //construir(s, chain[0], 0, 0, chain[0].size()-1);
-  for (size_t i = 0; i < 2*chain[3].size()-1; i++) {
-    std::cout <<  st[3][i] <<" ";
+  for (size_t i = 0; i < 2*chain[0].size()-1; i++) {std::cout <<  st[0][i] <<" ";}
+  for (size_t k = 0; k < q; k++) {
+    std::cin >> peticion;
+    if(peticion=='U'){
+      std::cin >> cambio;
+      std::cin >> valornuevo;
+      std::cout << A[cambio] << '\n';
+      actualizaHoja (st[numChain[cambio]], A[cambio], valornuevo);
+    }
+    if(peticion=='P'){
+
+    }
   }
+  for (size_t i = 0; i < 2*chain[0].size()-1; i++) {std::cout <<  st[0][i] <<" ";}
 
 
   return 0;
