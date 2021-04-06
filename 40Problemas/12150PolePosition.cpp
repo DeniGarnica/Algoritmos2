29a1fffffc6bfd1540224d0ecafdc5946946a38a
DeniGarnica <denisse.garnica@cimat.mx>
2021-04-05 23:10:13 -0500

    Problema con comentarios

--- 40Problemas/12150PolePosition.cpp
+++ 40Problemas/12150PolePosition.cpp
@@ -13,11 +13,11 @@ do{
   int B[n][2];
   for (int i = 0; i < n; i++) {
     std::cin >> c >> p;
-    aux1=i+p;
+    aux1=i+p; //la forma de calcular la posicion inicial es su posicion actual + los lugares que subio
     if(aux1>=n||aux1<0){
-      respuesta=-1;
+      respuesta=-1; //si nos da una posaicion fuera del rango no hay acomodo posible
     }else{
-      if(B[aux1][0]!=aux2){
+      if(B[aux1][0]!=aux2){ //con esto reviso si alguien en este caso ya tuvo asignada la misma posicion inicial
         B[aux1][0]=aux2;
         B[aux1][1]=c;
       }else{
@@ -25,7 +25,8 @@ do{
       }
     }
   }
-  if(respuesta!=-1 && n!= 0){
+  if(respuesta!=-1 && n!= 0){ // si hay una respuesta correcta imprimimos los casosas posi
+
     for (int i = 0; i < n; i++){
       std::cout << B[i][1];
       if(i<n-1){
