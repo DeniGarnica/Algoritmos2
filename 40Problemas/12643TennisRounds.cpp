2e08be5693f7a75f04fbb1f875de1f527dad2f93
DeniGarnica <denisse.garnica@cimat.mx>
2021-04-21 22:40:53 -0500

    Teniss Completo Con explicaciones

--- 40Problemas/12643TennisRounds.cpp
+++ 40Problemas/12643TennisRounds.cpp
@@ -3,21 +3,25 @@ using namespace std;
 #include <math.h>
 
 int main(){
-  int n, a, b, r1, r2 ,res;
+  int n, a, b, r1, r2 ,res,aux, e;
+  e=0;
   while(cin){
-  res=1;
+    if(e!=0)
+      std::cout  << '\n';
+  res=0;
+  aux=1;
+  e=1;
   std::cin >>n>>a>>b;
-  r1=ceil((a+a%2)/2);
-  r2=ceil((b+b%2)/2);
-  while(r1!=r2){
-    res++;\
-    if(r1!=0)
-      r1=ceil((r1+r1%2)/2);
-    if(r2!=0)
-      r2=ceil((r2+r2%2)/2);
+  while (aux) {
+    if(a!=b&&a!=0&&b!=0){   //Seguimos avanzando de ronda hasta que les toque estar en el mismo bloque
+      a=ceil((a+a%2)/2);
+      b=ceil((b+b%2)/2);
+      res++; //Si no estaban en el mismo bloque, paso una ronda mas
+    }else{
+      aux=0;
+    }
   }
-  std::cout << res << '\n';
-
+  std::cout << res;
   }
   return 0;
 }
--- 40Problemas/CodeJamWomen2021P1.cpp
+++ 40Problemas/CodeJamWomen2021P1.cpp
@@ -30,6 +30,5 @@ for (int i = 0; i < t; i++) {
   std::cout << "Case #"<< i+1<<": "<<0 << '\n';
 }
 }
-
   return 0;
 }
