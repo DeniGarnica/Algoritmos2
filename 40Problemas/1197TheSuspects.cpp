2912a196c7edb1c73e4cd60061c5a082534aa029
DeniGarnica <denisse.garnica@cimat.mx>
2021-04-21 22:59:46 -0500

    base unicamente

---
+++ 40Problemas/1197TheSuspects.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main(){
+int n, m;
+int e=0;
+while (cin) {
+  if(e!=0)
+    std::cout  << '\n';
+  e=1;
+  std::cin >> n>> m;
+}
+  return 0;
+}
