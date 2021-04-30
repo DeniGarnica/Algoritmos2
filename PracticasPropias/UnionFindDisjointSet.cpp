87d2ae2dc1af8037608b1cbdecbbd0b603e3b39a
DeniGarnica <denisse.garnica@cimat.mx>
2021-04-30 00:31:10 -0500

    Funcional

--- PracticasPropias/UnionFindDisjointSet.cpp
+++ PracticasPropias/UnionFindDisjointSet.cpp
@@ -9,23 +9,29 @@ struct Node{
   Node* parent;
 };
 
-map<int, Node> mapa; //Con esto guardamos el nodo asociado a cada dato
+map<int, Node*> mapa; //Con esto guardamos el nodo asociado a cada dato
+
+void print_map(std::map<int, Node> const &m){
+  for (auto it = m.cbegin(); it != m.cend(); ++it) {
+        std::cout << "{" << (*it).first << "}\n";
+    }
+}
+
 
 void Make_set(int a){
   Node* nodo = new Node();
   nodo->data = a;
   nodo->rank = 0;
   nodo->parent = nodo;
-  mapa.insert ( std::pair<int,Node>(a, *nodo) );
+  mapa.insert (std::pair<int,Node*>(a, nodo));
 }
 
 int root(int a){
   Node* nodo;
-  *nodo = mapa[a];
-  //std::cout << nodo->data << '\n';
+  nodo = mapa[a];
   while((nodo->parent)->parent!=nodo->parent){
     nodo->parent=nodo->parent->parent;
-    //std::cout << "1" << '\n';
+    std::cout << "while" << '\n';
   }
   return nodo->parent->data;
 }
@@ -39,34 +45,37 @@ void Union(int a, int b){
   int r_b=root(b);
   Node* raiz_a;
   Node* raiz_b;
-  *raiz_a = mapa[r_a];
-  *raiz_b = mapa[r_b];
+  raiz_a = mapa[r_a];
+  raiz_b = mapa[r_b];
 
   if(raiz_a->rank > raiz_b->rank){
     Node* aux= raiz_a;
     raiz_a=raiz_b;
     raiz_b=aux;
-  }else{
+  }
     raiz_a->parent = raiz_b;
     raiz_b->rank++;
-  }
 }
 }
 
 
 int main(){
   Make_set(1);
-  //std::cout << root(1) << '\n';
+  std::cout << root(1) << '\n';
   Make_set(2);
-  //std::cout << root(2) << '\n';
+  std::cout << root(2) << '\n';
   Make_set(3);
   Make_set(4);
   Make_set(5);
   Make_set(6);
   Make_set(7);
   Union(1, 2);
-  std::cout << root(2) << '\n';
+  std::cout << "root(2): " <<root(2) << '\n';
+  std::cout << "root(1): "<< root(1) << '\n';
   Union(2, 3);
+  std::cout << "root(3): "<< root(3) << '\n';
+  std::cout << "root(2): "<< root(2) << '\n';
+  std::cout << "root(1): "<< root(1) << '\n';
   Union(4, 5);
   Union(6, 7);
   Union(5, 6);
