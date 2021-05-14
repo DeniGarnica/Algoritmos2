fab989de7f43bad2721cb6851eed5192c11fa5b7
DeniGarnica <denisse.garnica@cimat.mx>
2021-05-14 00:36:56 -0500

    falta optimizar el mcd a euclides

--- 40Problemas/332RationalNumbersfromRepeatingFractions.cpp
+++ 40Problemas/332RationalNumbersfromRepeatingFractions.cpp
@@ -4,15 +4,29 @@
 #include <vector>
 using namespace std;
 
-void fraction(int k, int j){
-  
+int gcd(int a, int b){
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+
+void fraction(int k, int j, float x){
+  int a = ceil(pow(10, k+j)*x - pow(10, k)*x);
+  int b = pow(10, k+j)-pow(10, k);
+  int d = gcd(a, b);
+  a = int(a/d);
+  b = int(b/d);
+  std::cout << a<<"/"<<b << '\n';
 }
 
 int main(){
   int k, j, decimales, cont;
+  float x;
   char aux;
   string aux2;
   char A[9];
+  int caso = 1;
   std::cin >> j;
   while (j!=-1) {
     k=0; cont = 0;
@@ -23,8 +37,17 @@ int main(){
       cont++;
       aux= getchar();
     }
+    k=cont-j;
     A[cont]='\0';
-    std::cout << A << '\n';
+    x=float(stoi(A))/pow(10,cont);
+    cont=0;
+    while (A[cont]==0) {
+      x = x/10;
+      cont++;
+    }
+    std::cout << "Case "<< caso <<": ";
+    fraction(k, j, x);
+    caso++;
     std::cin >> j;
   }
   return 0;
