827526fa0a0ad2cc184a3f3c4abf091a1dc4892c
DeniGarnica <denisse.garnica@cimat.mx>
2021-06-23 01:13:51 -0500

    1

--- 40Problemas/332RationalNumbersfromRepeatingFractions.cpp
+++ 40Problemas/332RationalNumbersfromRepeatingFractions.cpp
@@ -4,19 +4,37 @@
 #include <vector>
 using namespace std;
 
-int gcd(int a, int b){
+int gcd(int a, int b)
+{
+  int g;
     if (a == 0)
-        return b;
-    return gcd(b % a, a);
+       return b;
+    if (b == 0)
+       return a;
+    if (a == b)
+        return a;
+    if (a > b){
+      while (a-b>b) {
+        a=a-b;
+      }
+      g = gcd(a-b, b);
+      return g;
+    }
+    while (b-a>a) {
+      b=b-a;
+    }
+    g = gcd(a, b-a);
+    return g;
 }
-
 
 void fraction(int k, int j, float x){
   int a = ceil(pow(10, k+j)*x - pow(10, k)*x);
   int b = pow(10, k+j)-pow(10, k);
+  std::cout << "a: "<<a << '\n';
+  std::cout << "b: "<<b << '\n';
   int d = gcd(a, b);
-  a = int(a/d);
-  b = int(b/d);
+  //a = int(a/d);
+  //b = int(b/d);
   std::cout << a<<"/"<<b << '\n';
 }
 
@@ -32,7 +50,7 @@ int main(){
     k=0; cont = 0;
     getchar(); getchar(); getchar();
     aux= getchar();
-    while (aux!='\n') {
+    while (aux>47&&aux<58) {
       A[cont]=aux;
       cont++;
       aux= getchar();
@@ -41,10 +59,10 @@ int main(){
     A[cont]='\0';
     x=float(stoi(A))/pow(10,cont);
     cont=0;
-    while (A[cont]==0) {
+    /*while (A[cont]=='0') {
       x = x/10;
       cont++;
-    }
+    }*/
     std::cout << "Case "<< caso <<": ";
     fraction(k, j, x);
     caso++;
