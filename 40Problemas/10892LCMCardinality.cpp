01f7057b71777fd8955cc4f51c9a4835bc95cdea
DeniGarnica <denisse.garnica@cimat.mx>
2021-06-29 00:23:55 -0500

    solo hice razonamiento basico en papel

---
+++ 40Problemas/10892LCMCardinality.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+  int n;
+  std::cin >> n;
+  while (n!=0) {
+
+
+    std::cin >> n;
+  }
+  return 0;
+}
