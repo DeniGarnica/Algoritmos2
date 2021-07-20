cd73896bb3cb9c0cafecb68fa816b6cfbfc7fbf2
DeniGarnica <denisse.garnica@cimat.mx>
2021-07-20 16:27:29 -0500

    cambio de primos, pero sigue sin ser suficiente

--- 40Problemas/10892LCMCardinality.cpp
+++ 40Problemas/10892LCMCardinality.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 typedef long long ll;
 
-ll p[150001]; //guarda primos en orden
-bool A[150001];
+ll p[90000000]; //guarda primos en orden
+bool A[90000000];
 
 ll gcd(ll a, ll b){
   ll g;
@@ -30,25 +30,35 @@ ll gcd(ll a, ll b){
     return g;
 }
 
-void primos(){
-  ll cont = 0;
-  for (size_t i = 2; i < 150001; i++) {
+ll contp = 0;
+ll primos(ll x){
+  for (size_t i = 2; i < 90000000; i++) {
     //std::cout << "i: "<< i << '\n';
     if(!A[i]){ //si no es compuesto
-      p[cont]=i;
+      p[contp]=i;
       cont++;
     }
-    for(ll j = 0; j < cont && p[j]*i<150001; j++){
+    for(ll j = 0; j < contp && p[j]*i<90000000; j++){
       //std::cout << "i*p[j]: "<<i*p[j] << '\n';
       A[i*p[j]] = 1; // a los compuestos de los primos hasta ahora generados les asigna 1
-      if (i%p[j]==0) //Si llegamos al mismo numero o su potencia 
+      if (i%p[j]==0) //Si llegamos al mismo numero o su potencia
         break;
     }
   }
 }
 
 ll descomposicion(int n){
-
+  ll k=0,cont,res=1;
+  while(n>1){
+      cont=0;
+      while(n%p[k]==0){
+          n/=p[k];
+          cont++;
+      }
+      res*=(cont*2)+1;
+      k++;
+  }
+  return res;
 }
 
 int main(){
@@ -59,8 +69,7 @@ int main(){
     if(n==1){
       std::cout << n <<" "<<1 << '\n';
     }else{
-      //ll res =  descomposicion(n);
-      ll res = 1;
+      ll res =  descomposicion(n);
       std::cout << n <<" "<< res/2 + 1 << '\n';
     }
     std::cin >> n;
