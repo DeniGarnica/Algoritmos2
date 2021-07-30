5a17b28571faf7a63d0a348e41b2ca82d951d6f6
DeniGarnica <denisse.garnica@cimat.mx>
2021-07-30 00:22:49 -0500

    Solo es la base vacia

---
+++ 40Problemas/634Polygon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int num_ver;
+  std::cin >> num_ver;
+  while (num_ver!=0) {
+  int X[num_ver], Y[num_ver];
+    for (size_t i = 0; i < num_ver; i++) {
+      std::cin >> X[i] >> Y[i];
+    }
+
+    std::cin >> num_ver;
+  }
+  return 0;
+}
