8a3be25b262acec45b67d1b0171f335311b4164c
DeniGarnica <denisse.garnica@cimat.mx>
2021-09-24 21:40:53 -0500

    buscar detalles

--- 40Problemas/11678CardsExchange.cpp
+++ 40Problemas/11678CardsExchange.cpp
@@ -1,13 +1,52 @@
 #include<bits/stdc++.h>
+#include <map>
 using namespace std;
+#define ll long long
+map<ll, ll> mapa;
+map<ll, ll> mapa2;
 
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  int num_casos, dato1, dato2;
-  std::cin >> num_casos;
-  for (int i = 0; i < num_casos; i++) {
-    std::cin >> dato1;
+  ll dato1, dato2, aux, cont1, cont2, uno;
+  std::cin >> dato1>> dato2;
+  while(dato1!=0 && dato2 != 0){
+    for (ll i = 0; i < dato1; i++) {
+      cont1=0;
+      cont2=0;
+      std::cin >> aux;
+      if (mapa.find(aux) == mapa.end()) {
+        uno = 1;
+        mapa.insert(pair<ll, ll>(aux, uno));
+      }else{
+          mapa[aux]++;
+      }
+    }
+    for (ll i = 0; i < dato2; i++) {
+      std::cin >> aux;
+      if (mapa2.find(aux) == mapa2.end() ) {
+          mapa2.insert(pair<ll, ll>(aux, uno));
+      }else{
+          mapa2[aux]++;
+      }
+    }
+    for (map<ll, ll>::iterator it = mapa.begin(); it != mapa.end(); ++it){
+      aux = it->first;
+      if (mapa2.find(aux) == mapa2.end()){
+        cont1++;
+      }
+    }
+    for (map<ll, ll>::iterator it = mapa2.begin(); it != mapa2.end(); ++it){
+      aux = it->first;
+      if (mapa.find(aux) == mapa.end()){
+        cont2++;
+      }
+    }
+    std::cout <<"ss" <<min(cont1, cont2) << '\n';
+    std::cin >> dato1>> dato2;
+    mapa.clear();
+    mapa2.clear();
   }
+
     return 0;
   }
