f7e2a4fd078f6b6bffbd2fb7a4744d8b74599ed8
DeniGarnica <denisse.garnica@cimat.mx>
2021-12-20 00:55:40 -0600

    Completo con comentarios.

--- 40Problemas/12347BinarySearchTree.cpp
+++ 40Problemas/12347BinarySearchTree.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+#include <queue>
 #define ll long long
 
 struct Node{
   int data;
-  struct Node *left, *right, *padre;
-}
+  struct Node *left, *right;
+};
 
 Node* newNode(int data){
   Node *temp = new Node;
@@ -14,45 +15,59 @@ Node* newNode(int data){
   return temp;
 }
 
+//Apartir del preorden construye recursivamente el arbol
+struct Node* construyeBST(int preorden[], int inicio, int fin){
+  if (inicio >  fin)
+    return NULL;
+
+  struct Node* node = newNode(preorden[inicio]);
+
+  int i;
+  for(i = inicio; i<= fin; i++){
+    if(preorden[i] > node->data)
+      break;
+  }
+
+  node->left = construyeBST(preorden, inicio + 1, i - 1);
+  node->right = construyeBST(preorden, i, fin);
+
+  return node;
+
+}
+//Dada la raiz imprime el arbol en postorden recursivamente
+void Postorden(Node* &current){
+    if (current->left!=NULL)
+        Postorden(current->left);
+
+    if (current->right!=NULL)
+       Postorden(current->right);
+
+    cout << current->data << '\n';
+}
+
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   int a;
-  bool primero = 1;
-  int actual = 0;
+  queue<int> datos;
 
-  while(cin>>a){
-    if(primero){
-      struct Node* raiz = newNode(a);
-      root->padre = raiz;
-      primero = 0;
-      actual = raiz;
-    }else{
-      if(a < actual->data){
-        struct Node* temp = newNode(a);
-        temp->padre = actual;
-        actual->left = temp;
-        actual = temp;
-      }else{
-        while(actual->padre->data < a || actual == root){
-          actual = actual->padre;}
-        if(actual->left!= NULL){
-          struct Node* temp = newNode(a);
-          temp->padre = actual;
-          actual->right = temp;
-          actual = temp;
-        }else{
-          actual = actual->padre;
-          struct Node* temp = newNode(a);
-          temp->padre = actual;
-          actual->right = temp;
-          actual = temp;
-        }
-      }
-    }
+  while (cin>>a) {
+    datos.push(a);
   }
 
+  int tam = datos.size();
+  int A[tam];
 
-
-    return 0;
+  //Agregamos los datos en un arreglo
+  for(int i = 0; i < tam; i++){
+    A[i] = datos.front();
+    datos.pop();
   }
+
+  struct Node* root = construyeBST(A, 0, tam-1);
+
+  //Imprimimos en Postorden
+  Postorden(root);
+
+  return 0;
+}
