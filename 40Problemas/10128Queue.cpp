03e42c111f8246c1fb132286fb5c10e9698fe1aa
DeniGarnica <denisse.garnica@cimat.mx>
2021-12-29 16:49:39 -0600

    Completo con comentarios. Nota del primer error: checar siempre en inicializar bien la matriz de referencia no se estaban poniendo los ceros bien.

--- 40Problemas/10128Queue.cpp
+++ 40Problemas/10128Queue.cpp
@@ -11,17 +11,27 @@ int main(){
     j se ven desde la derecha y
     k desde la izquierda */
   ll cola[15][15][15];
-  memset(cola, 0, sizeof(cola)); //Empezamos llenando los datos de ceros
+   //Empezamos llenando los datos de ceros
+  for (int i = 0; i < 15; i++)
+    for (int j = 0; j < 15; j++)
+      for (int k = 0; k < 15; k++)
+        cola[i][j][k] = 0;
   cola[1][1][1] = 1; //Creamos el caso base, que es de una persona
 
   for (int i = 2; i < 14; i++)
-    for (int j = 1; j < i; j++)
-      for (int k = 1; k < i; k++)
-        cola[i][j][k] = cola[i-1][j-1][k] + cola[i-1][j][k-1] + cola[i-1][j][k]*(i-2)
+    for (int j = 1; j <= i; j++)
+      for (int k = 1; k <= i; k++)
+        cola[i][j][k] = cola[i-1][j-1][k] + cola[i-1][j][k-1] + cola[i-1][j][k]*(i-2);
   /*
   Notemos que podemos dividir la cola en 3 casos,
   los cuales dependen de donde se encuentra la persona mas bajita de la cola
-  Si esta persona esta hasta delante, 
+  Sea una cola[i][j][k]
+  Si esta persona esta hasta delante, entonces si la quitamos tenemos una cola de i-1 personas,
+  donde se ven ahora solo j-1 desde la izquierda, y como no se veia desde la derecha, este numero se mantiene
+  Analogamente si esta persona esta atras, al quitarla tenemos una cola[i-1][j][k-1],
+  pues se redujo una persona, no se veia desde la izquierda por lo que es la misma cantidad,
+  y se ve una persona menos desde la derecha.
+  En caso de no estar en los extremos esta persona no se ve. Por lo que puede estar en (i-2) posibles lugares en medio
   */
   std::cin >> num_casos;
   for (int i = 0; i < num_casos; i++) {
