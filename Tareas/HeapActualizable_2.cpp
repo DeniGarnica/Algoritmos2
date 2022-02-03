d89f9644bbbf99e973ee433788b8264d96f0c322
DeniGarnica <denisse.garnica@cimat.mx>
2022-02-02 21:31:11 -0600

    Solo un caso mal

--- Tareas/HeapActualizable_2.cpp
+++ Tareas/HeapActualizable_2.cpp
@@ -22,11 +22,121 @@ template <class Key, class T> class UpdatableHeap{ //T es la clase de prioridade
         return 2*i+2;
     }
 
+    void intercambia(ll a, ll b){ //le pasamos los indices en el heap y los intercambia de posicion
+          pair<Key, T> aux = heap[a];
+          ll aux_map = mapa[heap[a].first];
+          mapa[heap[a].first] =  mapa[heap[b].first];
+          mapa[heap[b].first] = aux_map;
+          heap[a] = heap[b];
+          heap[b] = aux;
+    }
+    void Heapify_Down_to_Up(ll i){ //Checa si su padre es mayor y reacomoda, le pasamos el indice de donde va a checar.
+        if(i==0) //Si es la raiz, no hace nada.
+            return;
+        if(heap[Parent(i)].second< heap[i].second){ //Si el padre es menor, swap
+              intercambia(i, Parent(i));
+              Heapify_Down_to_Up(Parent(i));
+              return;
+        }  //Si tienen la misma prioriedad acomoda segun la clave
+        if(heap[Parent(i)].second == heap[i].second)
+              if(heap[Parent(i)].first < heap[i].first){
+                      intercambia(i, Parent(i));
+                      Heapify_Down_to_Up(Parent(i));
+              }
+
+    }
+    void Heapify_Up_to_Down(ll i){ //Checa si sus hijos son mayores
+        ll tam = heap.size();
+        if(tam < 2*i+2) //En caso de que no tenga ningun hijo
+              return;
+        if(tam > 2*i+2){ //Si tiene dos hijos
+              if(heap[i].second > heap[Left(i)].second && heap[i].second > heap[Right(i)].second) //Si es el mayor de los tres
+                    return;
+              if(heap[Left(i)].second > heap[i].second && heap[Left(i)].second > heap[Right(i)].second){ //Si el izquierdo es el mayor de los 3
+                    intercambia(i, Left(i));
+                    Heapify_Up_to_Down(Left(i));
+                    return;
+              }
+              if(heap[Right(i)].second > heap[i].second && heap[Right(i)].second > heap[Left(i)].second){ //Si el derecho es el mayor de los 3
+                    intercambia(i, Right(i));
+                    Heapify_Up_to_Down(Right(i));
+                    return;
+              }
+              if(heap[i].second > heap[Left(i)].second && heap[i].second == heap[Right(i)].second){ //Si es mayor que el izquierdo e igual que el otro
+                    if(heap[Right(i)].first > heap[i].first){
+                          intercambia(i, Right(i));
+                          Heapify_Up_to_Down(Right(i));
+                          return;
+                    }
+              }
+              if(heap[i].second > heap[Right(i)].second && heap[i].second == heap[Left(i)].second){ //Si es mayor que el dercho e igual que el otro
+                    if(heap[Left(i)].first > heap[i].first){
+                          intercambia(i, Left(i));
+                          Heapify_Up_to_Down(Left(i));
+                          return;
+                    }
+              }
+              if(heap[i].second == heap[Right(i)].second && heap[i].second == heap[Left(i)].second){ //Si todos tienen la misma prioriedad
+                    if(heap[Left(i)].first > heap[Right(i)].first && heap[Left(i)].first > heap[i].first){
+                          intercambia(i, Left(i));
+                          Heapify_Up_to_Down(Left(i));
+                          return;
+                    }
+                    if(heap[Right(i)].first > heap[i].first && heap[Right(i)].first > heap[i].first){
+                          intercambia(i, Right(i));
+                          Heapify_Up_to_Down(Right(i));
+                          return;
+                    }
+              }
+
+              if(heap[i].second < heap[Right(i)].second && heap[i].second < heap[Left(i)].second && heap[Right(i)].second == heap[Left(i)].second){ //Si es menor que ambos y los otros dos son iguales
+                    if(heap[Left(i)].first > heap[Right(i)].first){
+                          intercambia(i, Left(i));
+                          Heapify_Up_to_Down(Left(i));
+                          return;
+                    }
+                    intercambia(i, Right(i));
+                    Heapify_Up_to_Down(Right(i));
+                    return;
+              }
+
+        }else{ //Si tiene solo un hijo
+              if(heap[Left(i)].second < heap[i].second)
+                    return;
+              if(heap[Left(i)].second > heap[i].second){ //Si el hijo es mayor
+                    intercambia(i, Left(i));
+                    return;
+              }
+              if(heap[Left(i)].second == heap[i].second && heap[Left(i)].first < heap[i].first){ //Si tienen la misma prioriedad
+                    intercambia(i, Left(i));
+                    Heapify_Up_to_Down(Left(i));
+              }
+        }
+    }
+    void ActualizaI(ll i, T new_val){
+        T aux = heap[i].second;
+        heap[i].second = new_val;
+        if(aux > new_val){
+            Heapify_Up_to_Down(i); //Si el nuevo valor es menor, reacomoda con los hijos
+            return;
+        }
+        if(aux < new_val){
+            Heapify_Down_to_Up(i); //Si el nuevo valor es mayor, reacomoda con el padre
+            return;
+        }
+    }
+
   public:
     void imprimeTodo(){
         ll tam = heap.size();
         for(ll i=0;i<tam;i++)
-            cout << "index: " << mapa[heap[i].first]<< "  prioriedad: " << heap[i].second<< "  Clave: " << heap[i].first <<'\n';
+            if(mapa.count(heap[i].first)>0)
+              cout << "index: " << mapa[heap[i].first]<< "  prioriedad: " << heap[i].second<< "  Clave: " << heap[i].first <<'\n';
+    }
+    void imprimeMap(){
+      for (auto it = mapa.cbegin(); it != mapa.cend(); ++it) {
+          std::cout << "{" << (*it).first << ": " << (*it).second << "}\n";
+      }
     }
 
     bool emptyheap(){
@@ -35,30 +145,6 @@ template <class Key, class T> class UpdatableHeap{ //T es la clase de prioridade
         else
             return 0;
     }
-
-    pair<Key, T> top() const{
-        return heap[0];
-    }
-
-    void imprimeMap(){
-      for (auto it = mapa.cbegin(); it != mapa.cend(); ++it) {
-          std::cout << "{" << (*it).first << ": " << (*it).second << "}\n";
-      }
-    }
-
-    void insertOrUpdate(const Key k, const T p){
-        //mapa.count(k) nos da la cantidad de veces que esta el elemento k en el mapa.
-        if(mapa.count(k)>0){ //Si la clave ya esta, cambia el valor
-              int aux = mapa[k] ; //Nos da la posicion del elemento en el vector
-              //ActualizaI(aux, p);
-        }else{ //Si la clave no esta, insertala y ordena
-              ll t = heap.size();
-              mapa.insert(make_pair(k, t));
-              heap.push_back(make_pair(k, p));
-              //HeapifyDownUp(t);
-        }
-    }
-
     bool isInserted(const Key k){
           if(emptyheap()){
                 return 0;
@@ -69,29 +155,53 @@ template <class Key, class T> class UpdatableHeap{ //T es la clase de prioridade
                     return 0;
           }
     }
+    pair<Key, T> top() const{
+        return heap[0];
+
+    }
+
+    void insertOrUpdate(const Key k, const T p){
+        if(mapa.count(k)>0){ //Si la clave ya esta, cambia el valor //mapa.count(k) nos da la cantidad de veces que esta el elemento k en el mapa.
+              int aux = mapa[k] ; //Nos da la posicion del elemento en el vector
+              ActualizaI(aux, p);
+        }else{ //Si la clave no esta, insertala y ordena
+              ll t = heap.size();
+              mapa.insert(make_pair(k, t));
+              heap.push_back(make_pair(k, p));
+              Heapify_Down_to_Up(t);
+        }
+    }
 
     void eraseH(const Key K){//Le pasamos la clave y borra el elemento del heap.
           bool is = isInserted(K);
-          if(is == 0){//Si no esta insertado, no hace nada
+          if(is == 0)//Si no esta insertado, no hace nada
                 return;
+          if(heap.size()==1){//En caso de tener un unico elemento solo lo saca
+                heap.pop_back();
+                mapa.clear();
+                return;
+          } //En otro caso , intercambia el que se va a sacar y el ultimo, saca el ultimo, y reacomoda.
+          if(mapa[K]==heap.size()-1){//Si es el ultimo, solo lo saca
+                heap.pop_back();
+                auto it=mapa.find(K);
+                mapa.erase(it);
+                return;
+          }
+          //Si no es el ultimo, lo intercambia con el ultimo y lo saca.
+          ll aux = mapa[K];
+          T aux2 = heap[aux].second;
+          intercambia(mapa[K], heap.size()-1);
+          heap.pop_back();
+          auto it=mapa.find(K);
+          mapa.erase(it);
+          if(heap[aux].second >= aux2){ //Si la prioriedad del que estaba al ultimo, es mayor del que acaba de sacar, checa la relacion con su padre
+                if(aux!=0)
+                    Heapify_Down_to_Up(aux);
+                else
+                    Heapify_Up_to_Down(aux);
           }else{
-                if(heap.size()==1){//En caso de tener un unico elemento solo lo saca
-                      heap.pop_back();
-                      mapa.clear();
-                }else{ //En otro caso , intercambia el que se va a sacar y el ultimo, saca el ultimo, y reacomoda.
-
-                      if(mapa[K]!=heap.size()-1){ //Si no es el ultimo, lo intercambia con el ultimo y lo saca
-                            //intercambia(mapa[K], heap.size()-1)
-                            heap.pop_back();
-                            auto it=mapa.find(K);
-                            mapa.erase(it);
-
-                      }else{ //Si es el ultimo, solo lo saca
-                            heap.pop_back();
-                            auto it=mapa.find(K);
-                            mapa.erase(it);
-                      }
-                }
+                if(heap[aux].second < aux2) //Si la prioriedad del que estaba al ultimo, es menor de la prioriedad del que acab de sacar, checa la relacion con sus hijos
+                    Heapify_Up_to_Down(aux);
           }
     }
 
@@ -137,12 +247,11 @@ int main(){
             if(UHeap.isInserted(food))
                 UHeap.eraseH(food);
         }
-  std::cout << "heap actual despues de la accion " <<action << '\n';
-  UHeap.imprimeTodo();
-  std::cout << "mapa actual:" << '\n';
-  UHeap.imprimeMap();
-
-  std::cout  << '\n';
+  //std::cout << "heap actual despues de la accion " <<action << '\n';
+  //UHeap.imprimeTodo();
+  //std::cout << "mapa actual:" << '\n';
+  //UHeap.imprimeMap();
+  //std::cout  << '\n';
   }
   return 0;
 }
