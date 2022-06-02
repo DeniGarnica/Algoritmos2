0b6faacdc54f476c7f28a81fbc18dcda753c34e0
DeniGarnica <denisse.garnica@cimat.mx>
2022-06-02 16:08:24 -0500

    Completo con comentarios

--- 40Problemas/634Polygon.cpp
+++ 40Problemas/634Polygon.cpp
@@ -11,11 +11,17 @@ int main() {
       std::cin >> X[i] >> Y[i];
     }
     std::cin >> P_x>> P_y;
+    bool res = 0;
+    //Solo vamos checando si esta en los limites o no
     for(int i=0,j=num_ver-1;i<num_ver;j=i++){
-      if(Y[i]<=P_y+eps && P_y<Y[j] || Y[j]<=P_y+eps && P_y<Y[i]){
-        if()
+      if(Y[i]<=P_y+eps && P_y < Y[j] || Y[j] <= P_y+eps && P_y < Y[i]){
+        if(P_x - eps < (X[j] - X[i])*(P_y - Y[i])/(Y[j] - Y[i]) + X[i])
+          res^=1;
       }
     }
+
+    if(res) std::cout << "T" << '\n';
+    else std::cout << "F" << '\n';
 
     std::cin >> num_ver;
   }
